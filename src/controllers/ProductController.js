cf2914dca60ef83f4119158b1b95ed2592776454
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-10-24 20:14:14 -0300

    add status code in controllers

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,27 +5,27 @@ const Product = mongoose.model('Product');
 module.exports = {
   async index(req, res) {
     const products = await Product.find();
-    return res.json(products);
+    return res.status(200).json(products);
   },
   async store(req, res) {
     const product = await Product.create(req.body);
 
-    return res.json(product);
+    return res.status(201).json(product);
   },
   async show(req, res) {
     const product = await Product.find(req.params.id);
 
-    return res.json(product);
+    return res.status(200).json(product);
   },
   async update(req, res) {
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
-    return res.json(product);
+    return res.status(200).json(product);
   },
 
   async delete(req,res) {
     await Product.findByIdAndRemove(req.params.id, { new: true });
 
-    return res.send();
+    return res.status(200).send();
   }
 }
